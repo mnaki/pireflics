2e83bb65ba2fe288275446dc8d18c8dfce77ab7c
naki <nmohamed@student.42.fr>
2016-06-20 13:53:29 +0200

    refactor search

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -5,85 +5,27 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-// var readTorrent = require("read-torrent");
-// var nameToImdb = require("name-to-imdb");
-// var ptn = require('parse-torrent-name');
-var imdb = require('imdb');
-var omdb = require('omdb');
-var PageScraper = require('page-scraper');
+var TheMovieDb = require('themoviedb');
+var client = new TheMovieDb('67493736c8511d59d83f70c4b88a72f6');
+
+var paginate = function (list, page, itemPerPage) {
+	return _.slice(list, (page-1) * itemPerPage, (page) * itemPerPage);
+}
 
 module.exports = {
 	popular: function (req, res) {
-		var selector = '#main > div > span > div > div > div.lister > table > tbody > tr > td.titleColumn > a';
-		var pageScraper = new PageScraper({
-			baseUrl: 'http://www.imdb.com'
-		});
-		pageScraper.scrape('/chart/moviemeter', function(error, $) {
-			var titles = $(selector).map(function (i, el) {
-				return $(el).text();
-			}).get();
-
-			console.log(titles)
-
-			if (req.wantsJSON) return res.json(titles);
-			else               return res.send(titles);
+		client.popularMovies(function(err, movies) {
+			movies = paginate(movies, req.param('page') || 1, req.param('itemPerPage') || 5)
+			if (req.wantsJSON) return res.json(movies);
+			else               return res.send(movies);
 		});
 	},
 
-	/*
-	** GET http://localhost:1337/movie/search/naruto?&page=1&itemPerPage=2
-	** [
-	**	  {
-	**	    "title": "Naruto: Shippûden",
-	**	    "year": {
-	**	      "from": 2007
-	**	    },
-	**	    "imdb": "tt0988824",
-	**	    "type": "series"
-	**	  },
-	**	  {
-	**	    "title": "Naruto",
-	**	    "year": {
-	**	      "from": 2002,
-	**	      "to": 2007
-	**	    },
-	**	    "imdb": "tt0409591",
-	**	    "type": "series"
-	**	  }
-	**	]
-	*/
-
 	search: function (req, res) {
-		omdb.search(req.param('name'), function (err, movies) {
-			if (err) throw err;
-
-			var page = req.param('page') || 1
-			var itemPerPage = req.param('itemPerPage') || 5
-
-			sails.log.debug("movies " + movies)
-			movies = _.slice(movies, (page-1) * itemPerPage, (page) * itemPerPage)
-
-			var detailed = [];
-
-            movies.forEach(function (movie) {
-				sails.log.debug(movie)
-
-                imdb(movie.imdb, function(err, data) {
-                  if (err) sails.log.error(err.stack);
-
-                  if (data) {
-					  sails.log.debug("data " + data)
-                      detailed.push(data);
-                      if (detailed.length == movies.length) {
-						  sails.log.debug("sending response")
-                            if (req.wantsJSON) res.json(detailed);
-        		            else               res.send(detailed);
-                      }
-                  }
-                });
-            });
-
-
-		})
+		client.searchMovies({query: req.param('name'), sortBy: 'popularity.desc'}, function (err, movies) {
+			movies = paginate(movies, req.param('page') || 1, req.param('itemPerPage') || 5)
+			if (req.wantsJSON) return res.json(movies);
+			else               return res.send(movies);
+		});
 	}
 };
