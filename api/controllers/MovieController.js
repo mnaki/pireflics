9de5b64ce9b6c062afa769c6eb0b5cd0f63a6213
naki <naki.md@gmail.com>
2016-07-07 23:03:00 +0200

    rewrite

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -169,35 +169,37 @@ module.exports = {
 	},
 
 	play: function (req, res) {
-		async.waterfall([
-			function findMovie(cb) {
-				Movie.findOne({id: req.param('id')}).exec(cb)
-			},
-			function findComment(movie, cb) {
-				sails.log.debug(movie)
-				if (!movie.cast || movie.cast == {}) cb('Movie is still under process, try again later');
-				else Comment.find({movie_id: movie.id}).populate('user').exec(function (err, comments) {
-					return cb(err, movie, comments)
-				});
-			},
-			function findUser(movie, comments, cb) {
-				if (!comments) cb('No comments');
-				else User.findOne(req.session.user.id).exec(function (err, user) {
-					return cb(err, movie, comments, user)
-				});
-			},
-			function updateUser(movie, comments, user, cb) {
-				if (!user) cb('No user');
-				else
-				{
-					if (!user.movies) user.movies = [movie.id];
-					else              user.movies.push(movie.id);
+		async.waterfall(
+			[
+				function findMovie(cb) {
+					Movie.findOne({id: req.param('id')}).exec(cb)
+				},
+				function findComment(movie, cb) {
+					sails.log.debug(movie)
+					if (!movie.cast || movie.cast == {}) cb('Movie is still under process, try again later');
+					return Comment.find({movie_id: movie.id}).populate('user').exec(function (err, comments) {
+						return cb(err, movie, comments)
+					});
+				},
+				function findUser(movie, comments, cb) {
+					if (!comments) cb('No comments');
+					return User.findOne(req.session.user.id).exec(function (err, user) {
+						return cb(err, movie, comments, user)
+					});
+				},
+				function seeMovie(movie, comments, user, cb) {
+					if (!user) cb('No user');
+					user.movies = user.movies || [];
+					user.movies.push(movie.id);
+					return cb(null, movie, comments, user);
+				},
+				function saveUser(movie, comments, user, cb) {
 					user.save(function (err) {
-						if (err) cb(err);
 						return cb(err, { video: movie, comments: comments })
 					})
 				}
-			}], function (err, result) {
+			],
+			function (err, result) {
 				if (err) {
 					sails.log.debug(err)
 					req.session.msg = err;
@@ -209,46 +211,7 @@ module.exports = {
 				}
 			})
 	},
-/*
-	play: function (req, res) {
-		Movie.findOne({id: req.param('id')}).exec(function (err, movie) {
-			if (err || !movie) {
-				req.session.msg = err;
-				return res.redirect('/error');
-			}
-			if (!movie.cast || movie.cast == {})
-				return res.forbidden('Movie is still under process, try again later');
-			Comment.find({movie_id: movie.id}).populate('user').exec(function (err, comments) {
-				if (err || !comments) {
-					req.session.msg = err;
-					return res.redirect('/error');
-				}
-				User.findOne(req.session.user.id, function (err, user) {
-					if (err || !user) {
-							req.session.msg = err;
-							return res.redirect('/error');
-					}
-					if (!user.movies)
-						user.movies = [movie.id];
-					else
-						user.movies.push(movie.id);
-					user.save(function (err)
-					{
-						if (err) {
-							req.session.msg = err;
-							return res.redirect('/error');
-						}
-						// pretify the date
-						movie.release_date = moment(movie.release_date).fromNow();
-						// truncate the synopsis
-						movie.synopsis = _.truncate(movie.synopsis, { 'length': 500 });
-						return res.view('movie/play', { video: movie, comments: comments });
-					});
-				});
-			});
-		});
-	},
-*/
+
 	add_comment: function(req, res){
 		Comment.create({comment: req.param('comment'), user: req.session.user.id, movie_id: req.param('id')}).exec(function (err, result){
 			if (err || !result || result.length < 1) {
